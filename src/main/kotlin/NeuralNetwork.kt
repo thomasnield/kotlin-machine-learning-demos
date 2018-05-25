22bbd331ef2881ed5f77d6693879cc3f79f09554
Thomas Nield <thomasnield@live.com>
2018-05-24 22:18:56 -0500

    Neural Network structure

---
+++ src/main/kotlin/NeuralNetwork.kt
@@ -0,0 +1,146 @@
+import org.ojalgo.matrix.BasicMatrix
+import tornadofx.*
+import java.util.concurrent.ThreadLocalRandom
+
+fun main(args: Array<String>) {
+
+    val nn = neuralnetwork {
+        inputlayer(nodeCount = 3)
+        hiddenlayer(nodeCount =  3)
+        outputlayer(nodeCount = 2)
+
+    }
+
+    with(nn) {
+        input(1.0, 2.0, 3.0)
+
+        hiddenLayers.forEach {
+            it.valuesMatrix.apply(::println)
+        }
+        nn.outputLayer.valuesMatrix.apply(::println)
+    }
+
+}
+
+fun neuralnetwork(op: NeuralNetworkBuilder.() -> Unit): NeuralNetwork {
+    val nn = NeuralNetworkBuilder()
+    nn.op()
+    return nn.build().also { it.initialize() }
+}
+
+class NeuralNetwork(
+        inputNodeCount: Int,
+        hiddenLayerCounts: List<Int>,
+        outputLayerCount: Int
+) {
+
+    fun input(vararg values: Double) {
+        values.withIndex().forEach { (i,v) -> inputLayer.nodes[i].value = v }
+    }
+    val inputLayer = InputLayer(inputNodeCount)
+
+    val hiddenLayers = hiddenLayerCounts.asSequence()
+            .map {
+                CalculatedLayer(it)
+            }.toList().also { layers ->
+                layers.withIndex().forEach { (i,layer) ->
+                    layer.feedingLayer = (if (i == 0) inputLayer else layers[i-1])
+                }
+            }
+
+    val outputLayer = CalculatedLayer(outputLayerCount).also {
+        it.feedingLayer = (if (hiddenLayers.isNotEmpty()) hiddenLayers.last() else inputLayer)
+    }
+
+    fun initialize() {
+        hiddenLayers.forEach { it.initializeWeights() }
+        outputLayer.initializeWeights()
+        hiddenLayers.forEach { it.calculate() }
+        outputLayer.calculate()
+    }
+}
+
+
+
+// LAYERS
+sealed class Layer<N: Node>: Iterable<N> {
+    abstract val nodes: List<N>
+    override fun iterator() = nodes.iterator()
+}
+
+class InputLayer(nodeCount: Int): Layer<InputNode>() {
+
+    override val nodes = (0..(nodeCount-1)).asSequence()
+            .map { InputNode(it) }
+            .toList()
+}
+
+
+class CalculatedLayer(nodeCount: Int): Layer<CalculatedNode>() {
+
+    var feedingLayer: Layer<out Node> by singleAssign()
+
+    override val nodes by lazy {
+        (0..(nodeCount - 1)).asSequence()
+                .map { CalculatedNode(it, this) }
+                .toList()
+    }
+
+    var weightsMatrix: BasicMatrix = primitivematrix(0,0)
+    var valuesMatrix: BasicMatrix = primitivematrix(0,0)
+
+    fun initializeWeights() {
+        weightsMatrix = primitivematrix(count(), feedingLayer.count()) {
+            populate { row,col -> randomInitialValue() }
+        }
+    }
+
+    fun calculate() {
+        valuesMatrix = weightsMatrix * feedingLayer.toPrimitiveMatrix({it.value})
+    }
+}
+
+
+// NODES
+sealed class Node(val index: Int) {
+    abstract val value: Double
+}
+
+
+class InputNode(index: Int): Node(index) {
+    override var value = randomInitialValue()
+}
+
+
+class CalculatedNode(index: Int,
+                     val parentLayer: CalculatedLayer
+                 ): Node(index) {
+
+    override val value get() = parentLayer.valuesMatrix[index.toLong(),0].toDouble()
+
+}
+
+fun randomInitialValue() = ThreadLocalRandom.current().nextDouble(0.0,1.0)
+
+
+// BUILDERS
+class NeuralNetworkBuilder {
+
+    var input = 0
+    var hidden = mutableListOf<Int>()
+    var output = 0
+
+    fun inputlayer(nodeCount: Int) {
+        input = nodeCount
+    }
+
+    fun hiddenlayer(nodeCount: Int) {
+        hidden.add(nodeCount)
+    }
+
+    fun outputlayer(nodeCount: Int) {
+        output = nodeCount
+    }
+
+    fun build() = NeuralNetwork(input, hidden, output)
+}
