7ad41f94329aaed1f28a766acfeacf57274bedd3
e98594 <Nsswanogcbm26$>
2019-05-28 22:26:58 -0500

    wip

--- src/main/kotlin/TomNeuralNetwork.kt
+++ src/main/kotlin/TomNeuralNetwork.kt
@@ -160,8 +160,7 @@ class NeuralNetwork(
                 .sum() - 1
 
         sequenceOf(
-                generateSequence(80.0) { t -> t - .0005 }.takeWhile { it >= 0 },
-                generateSequence(120.0) { t -> t - .0005 }.takeWhile { it >= 0 }
+                generateSequence(80.0) { t -> t - .0005 }.takeWhile { it >= 0 }
         ).flatMap { it }.forEach { temp ->
 
             val randomVariableIndex = weightsPlusBiasesIndices.random()
